6d2762f1e5c67dbd2bedae0536d491bfaf74a169
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2020-06-15 00:26:48 +0530

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import axios from 'axios';
+import SearchBar from './SearchBar'
+import ImageList from './ImageList';
+
+class App extends React.Component {
+
+    state = { image: [] };
+
+    onSearchSubmit = async(term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID Sotiz7CgeN5RYqufsGzDEkTg83dFvFa-wkIrdEVrYBs'
+            }
+        });
+
+        this.setState({ image: response.data.results });
+
+        // .then(response => {
+        //     console.log(response.data.results);
+        // })
+        // console.log(term);
+    }
+
+    render() {
+        return ( <
+            div className = "ui container"
+            style = {
+                { marginTop: '20px' } } >
+            <
+            SearchBar onSubmit = { this.onSearchSubmit }
+            />
+            Found: { this.state.image.length }
+            images <
+            ImageList images = { this.state.image }
+            />  <
+            /div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/ImageList.js
@@ -0,0 +1,16 @@
+import './ImageList.css';
+import React from 'react';
+import ImageCard from './ImageCard';
+
+const ImageList = props => {
+    const images = props.images.map(image => {
+        // return <img src ={urls.regular} alt={description} key={id} />;
+        return <ImageCard key = { image.id }
+        images = { image }
+        />;
+    });
+
+    return <div className = "image-list" > { images } < /div>;
+};
+
+export default ImageList;
